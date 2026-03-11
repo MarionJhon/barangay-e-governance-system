7b0fda339e4a007102b1b8bd92b3da9ec100ba8d
Marion Jhon Agapay <marionagapay2001@gmail.com>
2026-03-12 00:01:47 +0800

    add pages for custodian of record

---
+++ app/(pages)/custodian-of-record/ordinance-resolution/page.tsx
@@ -0,0 +1,24 @@
+import AppSidebar from "@/components/layout/Sidebar";
+import { SidebarInset } from "@/components/ui/sidebar";
+import Header from "@/components/layout/Header";
+
+const OrdinanceResolutionPage = () => {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-400">
+      <AppSidebar />
+      <SidebarInset>
+        <Header />
+        <div className="flex flex-1 flex-col gap-4 p-4">
+          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+          </div>
+          <div className="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+        </div>
+      </SidebarInset>
+    </div>
+  );
+};
+
+export default OrdinanceResolutionPage;
